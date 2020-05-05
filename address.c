04e89e0d9603f8a050beccc3722f7de4856f6bdd
Oh gyuchan <gyu3637@gmail.com>
2020-05-05 14:41:54 +0900

    Add saveData func(address.c)

--- address.c
+++ address.c
@@ -6,3 +6,14 @@ int selectDataNum(Person *p, int count){
         scanf("%d", &index);
         return index;
 }
+void saveData(Person *p, int count){
+        FILE *fp = fopen("person.txt", "wt");
+        for(int i = 0; i < count; i++){
+                //이름 생년월일 전화번호 사는도시
+                if(p[i].dob.day != -1){
+                        fprintf(fp, "%s %4d%02d%02d %s %s\n", p[i].name, p[i].dob.year, p[i].dob.month, p[i].dob.day, p[i].phone, p[i].city);
+                }
+        }
+        fclose(fp);
+        printf("=> 파일저장완료!\n");
+}
