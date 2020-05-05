b38cf7323f04c2308d8e944a0d347449a319ad6c
Oh gyuchan <gyu3637@gmail.com>
2020-05-05 14:40:51 +0900

    Add selectDataNum func(address.c)

--- address.c
+++ address.c
@@ -1,1 +1,8 @@
-
+#include "address.h"
+int selectDataNum(Person *p, int count){
+        listPerson(p, count);
+        int index;      // index번호 저장
+        printf("선택할 번호 입력: ");
+        scanf("%d", &index);
+        return index;
+}
