7330c69fb1c7ec958d49c7042fdf4a71d3345988
Oh gyuchan <gyu3637@gmail.com>
2020-05-12 13:44:23 +0900

    Add void selectMenu func(addressbook.c)

--- addressbook.c
+++ addressbook.c
@@ -1,4 +1,21 @@
 #include "address.h"
+
+int selectMenu(){
+        int menu;
+
+        printf("\n=======전화번호부=======\n");
+        printf("(1) 전화번호부 목록 조회\n");
+        printf("(2) 전화번호부 목록 추가\n");
+        printf("(3) 전화번호부 목록 수정\n");
+        printf("(4) 전화번호부 목록 삭제\n");
+        printf("(6) 파일 저장\n");
+        printf("(0) 프로그램 종료\n");
+        printf("\n=> 메뉴 입력: ");
+        scanf("%d",&menu);
+
+        return menu;
+}
+
 int main(void){
 
 	Person plist[10];
